abb9bb159e2671320bcef9a161adcfbdda5cea45
sokolLonely <simonryab@gmail.com>
2026-02-25 21:27:06 -0700

    added inquirer and supply classes

--- src/App.java
+++
@@ -1,9 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-       try {
-        int x = 5/0;
-         } catch (ArithmeticException e) {
-            System.out.println("Cannot divide by zero");
-       }
-    }
-}
---
+++ src/edu/ucalgary/oop/Supply.java
@@ -0,0 +1,49 @@
+/**
+ * Class: Supply
+ * Date: Feb 25, 2026
+ * Completed by: Calvin Shaw
+ */
+
+package edu.ucalgary.oop;
+
+import java.lang.IllegalArgumentException;
+
+public class Supply {
+
+    private String type;
+    private int quantity;
+
+    /* Throws IllegalArgumentException if quantity is less than 0 */
+    public Supply(String type, int quantity) {
+
+        if (quantity < 0) {
+            throw new IllegalArgumentException();
+        }
+
+        this.type = type;
+        this.quantity = quantity;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    /* Throws IllegalArgumentException if quantity is less than 0 */
+    public void setQuantity(int quantity) {
+
+        if (quantity < 0) {
+            throw new IllegalArgumentException();
+        }
+        
+        this.quantity = quantity;
+    }
+
+    public String getType() {
+        return this.type;
+    }
+
+    public int getQuantity() {
+        return this.quantity;
+    }
+
+}
