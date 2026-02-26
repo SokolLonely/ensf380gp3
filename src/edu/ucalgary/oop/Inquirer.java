f1e67eec5fa01c54bbd3caa6a9c2e272f47988f1
calvshaw <clvnrl@gmail.com>
2026-02-26 13:04:00 -0700

    Removed check in Family relation. Fixed formatting

--- src/edu/ucalgary/oop/Inquirer.java
+++ src/edu/ucalgary/oop/Inquirer.java
@@ -2,31 +2,35 @@ package edu.ucalgary.oop;
 
 public class Inquirer {
     private final String FIRST_NAME;
- private final String LAST_NAME;
-private final String INFO;
-private final String SERVICES_PHONE;
-Inquirer(String firstName, String lastName, String phone, String info)
-{
-    this.FIRST_NAME =firstName;
-    this.LAST_NAME = lastName;
-    this.SERVICES_PHONE = phone;
-    this.INFO = info;
+    private final String LAST_NAME;
+    private final String INFO;
+    private final String SERVICES_PHONE;
 
-}
-String getFirstName()
-{
-    return this.FIRST_NAME;
-}
-String getLastName() 
-{
-    return this.LAST_NAME;
-}
-String getServicesPhoneNum()
-{
-    return this.SERVICES_PHONE;
-}
-String getInfo()
-{
-    return this.INFO;
-}
+    Inquirer(String firstName, String lastName, String phone, String info)
+    {
+        this.FIRST_NAME =firstName;
+        this.LAST_NAME = lastName;
+        this.SERVICES_PHONE = phone;
+        this.INFO = info;
+    }
+
+    String getFirstName()
+    {
+        return this.FIRST_NAME;
+    }
+    
+    String getLastName() 
+    {
+        return this.LAST_NAME;
+    }
+
+    String getServicesPhoneNum()
+    {
+        return this.SERVICES_PHONE;
+    }
+
+    String getInfo()
+    {
+        return this.INFO;
+    }
 }
--- src/edu/ucalgary/oop/ReliefService.java
+++ src/edu/ucalgary/oop/ReliefService.java
@@ -8,60 +8,71 @@ public class ReliefService {
     private LocalDate dateOfInquiry;
     private String infoProvided;
     private Location lastKnownLocation;
+    
     ReliefService(Inquirer inquirer, DisasterVictim missingPerson, LocalDate inquiryDate, String infoProvided, Location lastLocation)
     {
-    this.inquirer = inquirer;
-    this.missingPerson = missingPerson;
-    this.dateOfInquiry = inquiryDate;
-    this.infoProvided = infoProvided;
-    this.lastKnownLocation = lastLocation;
+        this.inquirer = inquirer;
+        this.missingPerson = missingPerson;
+        this.dateOfInquiry = inquiryDate;
+        this.infoProvided = infoProvided;
+        this.lastKnownLocation = lastLocation;
+    }
 
+    Inquirer getInquirer() 
+    {
+        return this.inquirer;
     }
-Inquirer getInquirer() 
-{
-    return this.inquirer;
-}
-void setInquirer(Inquirer inqurier)
-{
-    this.inquirer = inqurier;
-}
-DisasterVictim getMissingPerson() 
-{
-    return missingPerson;
-}
-void setMissingPerson(DisasterVictim missingPerson)
-{
-    this.missingPerson = missingPerson;
-}
-LocalDate getDateOfInquiry()
-{
-    return dateOfInquiry;
-}
-void setDateOfInquiry(LocalDate inquryDate) throws IllegalArgumentException
-{
-    if (inquryDate) {
-        throw new IllegalArgumentException("Date of inquiry cannot be null.");
+
+    void setInquirer(Inquirer inqurier)
+    {
+        this.inquirer = inqurier;
     }
-    this.dateOfInquiry = inquryDate;
-}
-String getInfoProvided()
-{
-    return infoProvided;
-}
-void setInfoProvided(String info)
-{
-    this.infoProvided = info;
-}
-Location getLastKnownLocation() 
-{
-    return lastKnownLocation;
-}
-void setLastKnownLocation(Location lastLocation)
-{
-    this.lastKnownLocation = lastLocation;
-}
-String getLogDetails() 
-{
-    return "Inquirer: " + inquirer.getFirstName() + " " + inquirer.getLastName() + ", Missing Person: " + missingPerson.getName() + ", Date of Inquiry: " + dateOfInquiry.toString() + ", Info Provided: " + infoProvided + ", Last Known Location: " + lastKnownLocation.getName();
-}
+
+    DisasterVictim getMissingPerson() 
+    {
+        return missingPerson;
+    }
+
+    void setMissingPerson(DisasterVictim missingPerson)
+    {
+        this.missingPerson = missingPerson;
+    }
+
+    LocalDate getDateOfInquiry()
+    {
+        return dateOfInquiry;
+    }
+
+    void setDateOfInquiry(LocalDate inquryDate) throws IllegalArgumentException
+    {
+        if (inquryDate) {
+            throw new IllegalArgumentException("Date of inquiry cannot be null.");
+        }
+        this.dateOfInquiry = inquryDate;
+    }
+
+    String getInfoProvided()
+    {
+        return infoProvided;
+    }
+
+    void setInfoProvided(String info)
+    {
+        this.infoProvided = info;
+    }
+
+    Location getLastKnownLocation() 
+    {
+        return lastKnownLocation;
+    }
+
+    void setLastKnownLocation(Location lastLocation)
+    {
+        this.lastKnownLocation = lastLocation;
+    }
+    
+    String getLogDetails() 
+    {
+        return "Inquirer: " + inquirer.getFirstName() + " " + inquirer.getLastName() + ", Missing Person: " + missingPerson.getName() + ", Date of Inquiry: " + dateOfInquiry.toString() + ", Info Provided: " + infoProvided + ", Last Known Location: " + lastKnownLocation.getName();
+    }
 } //         "Inquirer: John, Missing Person: Jane Alex, Date of Inquiry: 2025-02-10, Info Provided: Looking for family member,        Last Known Location:  University of Calgary"
