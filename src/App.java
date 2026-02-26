79f59efd0f51fb11be0244108529b5261eb2d9fc
sokolLonely <simonryab@gmail.com>
2026-02-25 19:06:34 -0700

    useless commit

--- src/App.java
+++ src/App.java
@@ -1,5 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       try {
+        int x = 5/0;
+         } catch (ArithmeticException e) {
+            System.out.println("Cannot divide by zero");
+       }
     }
 }
